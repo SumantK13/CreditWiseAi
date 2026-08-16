3751d3fe8b4034fe4e30a1615f73d749ccf8e237
Prathamesh Talekar <prathameshtalekar18@gmail.com>
2026-08-16 15:44:56 +0530

    Enhanced model with smotenC

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        .then(()=>console.log('MongoDB Connected!'))
-    }catch(err){
-        console.log("Mongodb connection failed");
+const connectDB = async () => {
+    try {
+        console.log("MONGO_URI:", process.env.MONGO_URI);
+
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "Creditwise"
+        });
+
+        console.log("MongoDB Connected!");
+        console.log("DB Name:", mongoose.connection.name);
+        console.log("Host:", mongoose.connection.host);
+
+    } catch (err) {
+        console.log("Mongodb connection failed", err);
         process.exit(1);
     }
-
-
 };
 
 export default connectDB;
